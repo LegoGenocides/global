90ee86532867c33c5c6233132694e8ccb2da4323
Colin <91840948+Colin4President@users.noreply.github.com>
2026-06-09 22:00:42 +0200

    Update GoofyList.lua

--- GoofyList.lua
+++ GoofyList.lua
@@ -2,6 +2,8 @@ local goofy = {
   1594196558;
   4519812724;
   8794215926;
+
+  [8990545679] = "bluds bio says: Account made specifically for obliterating unfair players in mm2. Reporting won't help 😛🌪🌧";
 }
 
 return goofy
